b6885d31ea834056c783d5fe7836815a66eaf01c
Viktor Josifov <viktor.josifov@efellows.bg>
2025-04-09 13:26:07 +0300

    Database init

--- db.py
+++ db.py
@@ -1,1 +1,8 @@
-from db import get_db
+import os
+from pymongo import MongoClient
+
+def get_db():
+    #Load Mongo URI from environment variables
+    client = MongoClient(os.getenv("MONGO_URI"))
+    db = client['online_exam_system'] #Database name
+    return db
