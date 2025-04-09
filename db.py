1d6242406f0e1e51c2ddfb7b00ea418e66e13ab9
Viktor Josifov <viktor.josifov@efellows.bg>
2025-04-09 14:12:53 +0300

    Fuck this shir

--- db.py
+++ db.py
@@ -2,18 +2,15 @@ import os
 from pymongo import MongoClient
 from dotenv import load_dotenv
 
-# Load environment variables from .env file
+
 load_dotenv()
 
-# Connect to MongoDB
-def get_db():
-    # Get the MongoDB URI from the environment variable
-    mongo_uri = os.getenv('MONGO_URI')
+uri = os.getenv("MONGO_URI")
+client = MongoClient(uri)
 
-    if not mongo_uri:
-        raise ValueError("Mongo URI is missing in .env file")
-
-    # Connect to the MongoDB Atlas cluster
-    client = MongoClient(mongo_uri)
-    db = client['OnlineExamSystem']
-    return db
+try:
+    client.admin.command('ping')
+    print("✅ Connected to MongoDB Atlas!")
+except Exception as e:
+    print("❌ Connection failed:")
+    print(e)
